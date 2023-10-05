7796066fef78b2c7321291e54e2b6b7d82fa9ca1
Dajan Medina <dmedina2150dev@gmail.com>
2023-10-05 19:47:04 -0300

    [Add]: Tipos basicos de TypeScript

--- 01-reforzamiento/src/typescript/tipos-basicos.tsx
+++ 01-reforzamiento/src/typescript/tipos-basicos.tsx
@@ -1,9 +1,20 @@
 
 
 export const TiposBasicos = () => {
+
+  const nombre: string  = 'Fernando';  
+  const edad: number = 35;
+
+  const estaActivo: boolean = false;
+
+  const poderes = ['Velocidad', 'Volar', 'Respirar en el agua'];
+
   return (
     <>
         <h3>Tipos basicos</h3>
+        { nombre.toLocaleUpperCase() }, { edad }, { (estaActivo) ? 'Activo' : 'NO está activo' }
+        <br />
+        { poderes.join(', ') }
     </>
   )
 }
